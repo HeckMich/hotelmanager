cf94b9d251b58bde30b4b3b05ceaa625d209b032
HeckMich <2310859029@fh-burgenland.at>
2024-05-25 10:50:59 +0000

    test

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test22222");
         /* TO DO
             -) Connect Database
             -) Klasse zur Tabelle erstellen!
