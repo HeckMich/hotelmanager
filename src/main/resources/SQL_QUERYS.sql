73e32cd70700950d789483be883be49ca5ad9872
LeonFHBurgenland <2310859017@fh-burgenland.at>
2024-05-26 15:31:19 +0000

    SQL QUERYS WIP

--- src/main/resources/SQL_QUERYS.sql
+++ src/main/resources/SQL_QUERYS.sql
@@ -5,3 +5,18 @@ select d.maintenance_type ,start_date, end_date, room_nr, c.name
 	join job c on b.job_id = c.job_id
 	join maintenance_type d on a.m_type_id = d.m_type_id
 where '2024-04-07' between start_date and end_date
+
+--Query 2
+select count(*) number_booked_rooms,
+(Select count(*) from room) - count(*)  number_empty_rooms,
+sum(cost) expected_earnings,
+(Select sum(cost) from room) - sum(cost) lost_earnings
+	from reservation a
+	join room b on a.room_nr = b.room_nr
+where '2024-07-09' between start_date and end_date
+
+--Query 3 --NOT FINISHED!!
+select *
+	from reservation a
+	right outer join room b on a.room_nr = b.room_nr
+where '2024-07-09' between start_date and end_date
