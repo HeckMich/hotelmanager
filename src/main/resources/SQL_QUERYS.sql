4655f971b5f63749fc7d385144d63bb23a24cc05
LeonFHBurgenland <2310859017@fh-burgenland.at>
2024-06-02 16:22:38 +0000

    SQL Select updated

--- src/main/resources/SQL_QUERYS.sql
+++ src/main/resources/SQL_QUERYS.sql
@@ -1,10 +1,9 @@
 --Query 1 (job_type is placeholder)
-select d.maintenance_type ,start_date, end_date, room_nr, c.name
-	from maintenance a
-	join can_do_maintenance b on a.m_type_id = b.m_type_id
-	join job c on b.job_id = c.job_id
-	join maintenance_type d on a.m_type_id = d.m_type_id
-where '2024-04-07' between start_date and end_date
+select b.maintenance_type ,start_date, end_date, room_nr, c.first_name, c.last_name
+	from planned_maintenance a
+	join maintenance_type b on a.m_type_id = b.m_type_id
+	join employee c on a.employee_id = c.employee_id
+where '2024-04-07' between start_date and end_date;
 
 --Query 2
 select count(*) number_booked_rooms,
